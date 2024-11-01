774c7a02ec78ceee8be22e44c4e3e64261c100dd
tmothymartel <t.m.martel@gmail.com>
2024-11-01 13:32:02 -0400

    added player

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 # Import game constants
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -12,15 +13,17 @@ def main():
     #clock object to help set FPS
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0),)
+        player.draw(screen)
         pygame.display.flip()
         # limit framerate to 60FPS
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
