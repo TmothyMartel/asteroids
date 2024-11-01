c8104dee7925dfe88057897745166b065019b4da
tmothymartel <t.m.martel@gmail.com>
2024-11-01 14:05:33 -0400

    grouped objects

--- main.py
+++ main.py
@@ -12,15 +12,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     #clock object to help set FPS
     clock = pygame.time.Clock()
-    dt = 0
+    dt = 0   
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    #game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0),)
-        player.draw(screen)
-        player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill((0,0,0))
+        
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         # limit framerate to 60FPS
         dt = clock.tick(60) / 1000
